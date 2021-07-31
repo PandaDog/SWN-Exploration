27100374c53bbd4a35233e9751bd0dcb27351c21
Rob McKay <rgmckay7@gmail.com>
2021-07-31 14:57:56 -0700

    Adding in some of the functionality of pulling from the tables.

--- SWN Generator.py
+++ SWN Generator.py
@@ -10,6 +10,7 @@ __email__ = "rgmckay@nevada.unr.edu"
 
 ##IMPORTS##
 import sys
+import os
 import random
 import math
 import pprint
@@ -17,30 +18,86 @@ import json
 import easygui
 
 
-def selectJsonFile():
-    filePath = easygui.fileopenbox(
-        title="Select Json File", filetypes="\*.json")
-    return filePath
+def loadJson(filepath):
+    with open(filepath, 'r') as json_file:
+        tmp = json_file.read()
+        table = json.loads(tmp)
+        return table
+        # Example Macro &{template:default} {{name=Shift fires his gun!}} {{Laser Pistol:=[[d20+4]] vs AC}}  {{Damage:=[[1d6+1]]}}
 
 
-def loadJson(filePath):
-    with open(filePath, 'r', encoding="UTF-8") as json_file:
-        data = json_file.read()
-        structure = json.loads(data)
-        return structure
-    # Example Macro &{template:default} {{name=Shift fires his gun!}} {{Laser Pistol:=[[d20+4]] vs AC}}  {{Damage:=[[1d6+1]]}}
+def printJsonTable(table):
+    print(json.dumps(table, indent=1))
 
 
 def Generate_Roll20_Macro(message):
     print("&{template:default} {{" + message + "}}" + '\n')
 
 
-def main():
-    print("Select your Json file.")
-    jsonPath = selectJsonFile()
-    data = loadJson(jsonPath)
-    print(json.dumps(data, indent=1))
+def generateProblem():
+    twist = random.choice(problems['problem']['twists'])
+    restraint = random.choice(problems['problem']['restraints'])
+    print(restraint + '. ' + twist)
 
+
+def generatePatron():
+    trust = random.choice(patrons['Trustworthiness'])
+    challenge = random.choice(patrons['Challenge'])
+    patronForce = random.choice(patrons['Force'])
+    eager = random.choice(patrons['Eagerness'])
+    reward = random.choice(patrons['Potential_Non-Cash_Rewards'])
+    print(trust + ' ' + challenge + ' ' +
+          patronForce + ' ' + eager + ' ' + reward)
+
+
+def generatePlace():
+    returnString = "Taco"
+    return returnString
+
+
+def generateUrbanEncounter():
+    venue = random.choice(urbanEncounters['urbanEncounters']['generalVenue'])
+    reasonInvolved = random.choice(
+        urbanEncounters['urbanEncounters']['reasonInvolved'])
+    eventNature = random.choice(
+        urbanEncounters['urbanEncounters']['eventNature'])
+    conflictReason = random.choice(
+        urbanEncounters['urbanEncounters']['conflict'])
+    atagonist = random.choice(urbanEncounters['urbanEncounters']['antagonist'])
+    print(venue + ' ' + reasonInvolved + ' ' + eventNature +
+          ' ' + conflictReason + ' ' + atagonist)
+
+
+def generateWildEncounter():
+    weather = random.choice(
+        wildEncounters['wildernessEncounters']['weatherLighting'])
+    nature = random.choice(
+        wildEncounters['wildernessEncounters']['nature'])
+    friend = random.choice(
+        wildEncounters['wildernessEncounters']['friendlies'])
+    ranges = random.choice(
+        wildEncounters['wildernessEncounters']['range'])
+    hostiles = random.choice(
+        wildEncounters['wildernessEncounters']['hostiles'])
+    print(weather + ' ' + nature + ' ' + friend +
+          ' ' + ranges + ' ' + hostiles)
+
+
+problems = loadJson("problem.json")
+patrons = loadJson("patrons.json")
+places = loadJson("places.json")
+NPC = loadJson("npc.json")
+wildEncounters = loadJson("wildernessEncounters.json")
+urbanEncounters = loadJson("urbanEncounters.json")
+conflictTypes = loadJson("conflictType.json")
+simpleNPC = loadJson("onerollNPC.json")
+
+
+def main():
+    generateProblem()
+    generatePatron()
+    generateUrbanEncounter()
+    generateWildEncounter()
 
     # If not importing. run main function
 if __name__ == '__main__':
